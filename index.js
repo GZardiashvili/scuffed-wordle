a1eb211292eb60f7912bf174f7b3da6b9364880c
GZardiashvili <zardiashviligiorgi17@gmail.com>
2022-09-08 08:58:19 +0400

    add keyboard stub

--- index.js
+++ index.js
@@ -18,12 +18,10 @@ let secret = wordList[randomIndex];
 let currentAttempt = '';
 let history = [];
 
-let grid = document.getElementById('grid');
-buildGrid();
-updateGrid();
-window.addEventListener('keydown', handleKeyDown);
-
 function handleKeyDown(e) {
+  if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) {
+    return;
+  }
   if (e.key === 'Backspace') {
     currentAttempt = currentAttempt.slice(0, -1);
   } else if (e.key === 'Enter') {
@@ -80,19 +78,77 @@ function drawAttempt(row, attempt, isCurrent) {
     if (isCurrent) {
       cell.style.backgroundColor = getBgColor(attempt, i);
     } else {
-      cell.style.backgroundColor = '#111';
+      cell.style.backgroundColor = BLACK;
     }
   }
 }
+
+let BLACK = '#111';
+let GREY = '#212121';
+let LIGHTGREY = '#888';
+let GREEN = '#538d4e';
+let YELLOW = '#b59f3b';
 
 function getBgColor(attempt, i) {
   let correctLetter = secret[i];
   let attemptLetter = attempt[i];
   if (attemptLetter === undefined || secret.indexOf(attemptLetter) === -1) {
-    return '#212121';
+    return GREY;
   }
   if (attemptLetter === correctLetter) {
-    return '#538d4e';
+    return GREEN;
   }
-  return '#b59f3b';
+  return YELLOW;
 }
+
+function buildKeyboard() {
+  buildKeyboardRow('qwertyuiop', false);
+  buildKeyboardRow('asdfghjkl', false);
+  buildKeyboardRow('zxcvbnm', true);
+}
+
+function buildKeyboardRow(letters, isLastRow) {
+  let row = document.createElement('div');
+  if (isLastRow) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.textContent = 'Enter';
+    button.style.backgroundColor = LIGHTGREY;
+    button.onClick = () => {
+      // currentAttempt += letter;
+      // updateGrid();
+    };
+    row.appendChild(button);
+  }
+  for (let letter of letters) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.textContent = letter;
+    button.style.backgroundColor = LIGHTGREY;
+    button.onClick = () => {
+      // currentAttempt += letter;
+      // updateGrid();
+    };
+    row.appendChild(button);
+  }
+  if (isLastRow) {
+    let button = document.createElement('button');
+    button.className = 'button';
+    button.textContent = 'Backspace';
+    button.style.backgroundColor = LIGHTGREY;
+    button.onClick = () => {
+      // currentAttempt += letter;
+      // updateGrid();
+    };
+    row.appendChild(button);
+  }
+  keyboard.appendChild(row);
+}
+
+let grid = document.getElementById('grid');
+let keyboard = document.getElementById('keyboard');
+
+buildGrid();
+buildKeyboard();
+updateGrid();
+window.addEventListener('keydown', handleKeyDown);
